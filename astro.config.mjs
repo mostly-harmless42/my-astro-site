d023ccc0c528c5592fb6c93694de564a54a4fc34
Will Read <wrgread1@gmail.com>
2025-12-23 22:41:10 -0800

    add deploy.yml, edit astro.configs.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site : 'https://mostly-harmless42.github.io',
+    base: '/my-astro-site'
+});
