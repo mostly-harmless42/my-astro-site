0a81a198d8ce01a494025e4f3367d01d262b6cfd
Will Read <wrgread1@gmail.com>
2025-12-23 22:49:37 -0800

    build site and remove /dist from .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site : 'https://mostly-harmless42.github.io',
-    base: '/my-astro-site'
+    base: '/my-astro-site/'
 });
