f6811e50430589d1eb756394080eebd2c9ba8408
Will Read <wrgread1@gmail.com>
2026-01-02 10:44:45 -0800

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site : 'https://mostly-harmless42.github.io',
-    base: '/my-astro-site/'
+    site : 'https://armouredregalia.com',
 });
